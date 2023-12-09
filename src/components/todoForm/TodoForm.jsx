05640ee4e4545da27d11adf49d5db3c4baf82eb8
amethystcrystal <crystalbardowell>
2023-12-09 14:32:39 -0500

    created a todo from component

--- src/components/todoForm/TodoForm.jsx
+++ src/components/todoForm/TodoForm.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function TodoForm() {
-  return (
-    <form className='TodoForm'>
-      <input type='text' className='Todo-Input' placeholder='Tasks for the day!'></input>
-    </form>
-  );
+    const [value, setValue] = useState("")
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(value)
+    }
+
+    return (
+        
+        <form className='todo-Form' onSubmit={handleSubmit}>
+           <h1>Todo List</h1><br></br>
+            <input type='text' className='todo-input' 
+            placeholder='Tasks for the day!' onChange={(e) => setValue(e.target.value)}></input>
+            <button type='submit' className='todo-btn'>Add Task</button>
+        </form>
+    );
 }
