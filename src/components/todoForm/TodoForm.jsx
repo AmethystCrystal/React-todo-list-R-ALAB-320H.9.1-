ff805d1ca6d1d03cc60ef1e8bb8dc88d16409d17
amethystcrystal <crystalbardowell>
2023-12-09 11:10:37 -0500

    created component files

---
+++ src/components/todoForm/TodoForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TodoForm() {
+  return (
+    <form className='TodoForm'>
+      <input type='text' className='Todo-Input' placeholder='Tasks for the day!'></input>
+    </form>
+  );
+}
