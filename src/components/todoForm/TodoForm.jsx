2c929d2476ae6f9fa51fbd7a30e7f813165ed6db
amethystcrystal <crystalbardowell>
2023-12-09 17:00:42 -0500

    updated todo form component

--- src/components/todoForm/TodoForm.jsx
+++ src/components/todoForm/TodoForm.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'value':
+            return { ...state, value: action.payload };
+            default:
+                throw new Error();
+    }
+}
+
+const ACTION ={
+    VALUE: 'value',
+}
 
 export default function TodoForm() {
-    const [value, setValue] = useState("")
+    const [state, dispatch] = useReducer(reducer, { value: "",  })
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        console.log(value)
+        console.log(state.value)
     }
 
     return (
@@ -14,7 +27,7 @@ export default function TodoForm() {
         <form className='todo-Form' onSubmit={handleSubmit}>
            <h1>Todo List</h1><br></br>
             <input type='text' className='todo-input' 
-            placeholder='Tasks for the day!' onChange={(e) => setValue(e.target.value)}></input>
+            placeholder='Tasks for the day!' value={state.value} onChange={(e) => dispatch({ type: ACTION.VALUE, payload: e.target.value})}></input>
             <button type='submit' className='todo-btn'>Add Task</button>
         </form>
     );
