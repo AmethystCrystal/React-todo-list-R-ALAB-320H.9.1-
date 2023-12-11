fb81b6187c5396c6072df794d7f906742b085366
AmethystCrystal <140659819+AmethystCrystal@users.noreply.github.com>
2023-12-11 14:41:18 -0500

    edited todoform

--- src/components/todoForm/TodoForm.jsx
+++ src/components/todoForm/TodoForm.jsx
@@ -15,18 +15,18 @@ const reducer = (state, action) => {
 
 const ACTION = {
     VALUE: 'value',
+    TOGGLE_COMPLETION: 'toggle_completion',
 }
 
 const predefinedItems = [
-    { id: 1, value: 'Take out garbage' },
-    { id: 2, value: 'Do dishes' },
-    { id: 3, value: 'Sweep house' },
-    { id: 4, value: 'Clean room' },
+    { id: 1, value: 'Take out garbage', completed: false },
+    { id: 2, value: 'Do dishes', completed: false },
+    { id: 3, value: 'Sweep house', completed: false },
+    { id: 4, value: 'Clean room', completed: false },
 ];
 
 export default function TodoForm() {
-    const [state, dispatch] = useReducer(reducer, { value: "", })
-    const [items, setItems] = useState(predefinedItems);
+    const [state, dispatch] = useReducer(reducer, { value: "", items: predefinedItems })
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -36,19 +36,23 @@ export default function TodoForm() {
 
         const item = {
             id: Math.floor(Math.random() * 1000),
-            value: state.value
+            value: state.value,
+            completed: false,
         };
 
-        setItems(oldList => [item, ...oldList]);
         dispatch({ type: ACTION.VALUE, payload: '' });
-
-        console.log(items);
+        dispatch({ type: ACTION.TOGGLE_COMPLETION, payload: item.id });
     }
 
     const deleteItem = (id) => {
-        const newArray = items.filter(item => item.id !== id);
-        setItems(newArray);
+        const newArray = state.items.filter(item => item.id !== id);
+        dispatch({ type: ACTION.VALUE, payload: '' });
+        dispatch({ type: ACTION.TOGGLE_COMPLETION, payload: id });
     }
+
+    const toggleCompletion = (id) => {
+        dispatch({ type: ACTION.TOGGLE_COMPLETION, payload: id });
+    };
 
     return (
 
@@ -59,14 +63,17 @@ export default function TodoForm() {
             <button onClick={() => addItem()} type='submit' className='todo-btn'>Add Task</button>
 
             <ul>
-                {items.map(item => {
+                {state.items.map((item) => {
                     return (
+
                         <li key={item.id}>
                             <input type='checkbox' checked={item.completed} onChange={() => toggleCompletion(item.id)} />
-                            {item.value} <button onClick={() => deleteItem(item.id)}>Delete</button> </li>
+                            {item.value} {''}  {!item.completed && (
+                                <button onClick={() => deleteItem(item.id)}>Delete</button>
+                            )}
+                        </li>
                     )
                 })}
-
             </ul>
         </form>
 
